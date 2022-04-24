0c41c580b619a51e553c4dd15852839a75ede801
vedant bharti <39922381+vedantbharti@users.noreply.github.com>
2022-04-25 00:38:47 +0530

    Time: 31 ms (38.49%), Space: 24 MB (29.70%) - LeetHub

--- 695-max-area-of-island/695-max-area-of-island.cpp
+++ 695-max-area-of-island/695-max-area-of-island.cpp
@@ -1,31 +1,28 @@
 class Solution {
     
-int findMaxArea(vector<vector<int>>& grid, int m, int n, vector<vector<int>>& visited, int i, int j){
-    if(i<0 || i>=m || j<0 || j>=n || grid[i][j]==0 || visited[i][j]==1){
+int findMaxArea(int i, int j, vector<vector<int>> &grid, vector<vector<int>> &visited){
+    int m = grid.size(), n = grid[0].size();
+    if(i<0 || i==m || j<0 || j==n || grid[i][j]==0 || visited[i][j]==1){
         return 0;
     }
-    visited[i][j] = 1;
-    return findMaxArea(grid,m,n,visited,i+1,j)+findMaxArea(grid,m,n,visited,i-1,j)+findMaxArea(grid,m,n,visited,i,j+1)+findMaxArea(grid,m,n,visited,i,j-1)+1;
     
+    visited[i][j] = 1;
+    return findMaxArea(i-1,j,grid,visited)+findMaxArea(i+1,j,grid,visited)+findMaxArea(i,j-1,grid,visited)+findMaxArea(i,j+1,grid,visited)+1;
 }
     
 public:
     int maxAreaOfIsland(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();
         vector<vector<int>> visited(m,vector<int>(n,0));
-        int max_area = 0;
+        int ans = 0;
         for(int i=0;i<m;i++){
             for(int j=0;j<n;j++){
-                if(visited[i][j]==0 && grid[i][j]==1) max_area = max(findMaxArea(grid,m,n,visited,i,j), max_area);
+                if(grid[i][j]==1 && visited[i][j]==0){
+                    ans = max(ans,findMaxArea(i,j,grid,visited));
+                }
             }
         }
         
-        // for(int i=0;i<m;i++){
-        //     for(int j=0;j<n;j++){
-        //         cout<<visited[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        return max_area;
+        return ans;
     }
 };
