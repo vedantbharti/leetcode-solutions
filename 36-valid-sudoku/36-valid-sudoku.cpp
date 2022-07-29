6d6adc3e790a1df3e2633a5718b453550c1ef309
vedant bharti <39922381+vedantbharti@users.noreply.github.com>
2022-07-29 09:33:46 +0530

    Time: 47 ms (26.29%), Space: 19.1 MB (53.18%) - LeetHub

--- 36-valid-sudoku/36-valid-sudoku.cpp
+++ 36-valid-sudoku/36-valid-sudoku.cpp
@@ -1,37 +1,32 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-        unordered_set<char> uset;
-        for(int i=0;i<9;i++){
-            uset.clear();
-            for(int j=0;j<9;j++){
-                if(uset.find(board[i][j])!=uset.end()){
-                    return false;
-                }
-                if(board[i][j]!='.') uset.insert(board[i][j]);
-            }
-        }
+        unordered_set<int> uset;
         
-        for(int j=0;j<9;j++){
-            uset.clear();
-            for(int i=0;i<9;i++){
-                if(uset.find(board[i][j])!=uset.end()){
-                    return false;
-                }
-                if(board[i][j]!='.') uset.insert(board[i][j]);
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                if(board[i][j]!='.' && uset.find(board[i][j])!=uset.end()) return false;
+                uset.insert(board[i][j]);
             }
+            uset.clear();
         }
         
         for(int i=0;i<9;i++){
             for(int j=0;j<9;j++){
-                int a = (j/3) + (i/3)*3;
-                int b = (j%3) + (i%3)*3;
-                if(a%3==0 && b%3==0) uset.clear();
-                if(uset.find(board[a][b])!=uset.end()){
-                    return false;
-                }
-                if(board[a][b]!='.') uset.insert(board[a][b]);
+                if(board[j][i]!='.' && uset.find(board[j][i])!=uset.end()) return false;
+                uset.insert(board[j][i]);
             }
+            uset.clear();
+        }
+        
+        for(int i=0;i<9;i++){
+            for(int j=0;j<9;j++){
+                int a = j/3 + (i/3)*3;
+                int b = j%3 + (i%3)*3;
+                if(board[a][b]!='.' && uset.find(board[a][b])!=uset.end()) return false;
+                uset.insert(board[a][b]);
+            }
+            uset.clear();
         }
         
         return true;
